72585f516bb0b03b1f9e2d97c4738c8355550546
UniversumGames <programming@universegame.de>
2021-04-01 18:27:14 +0200

    first version done

--- src/config.hpp
+++ src/config.hpp
@@ -1,2 +1,10 @@
+#pragma once
+
 #define CCS811_ADDR 0x5A
 #define SEALEVELPRESSURE_HPA (1013.25)
+
+#define UPLINK_SERVER_ADDRESS "raspi.universum.net" //(my own local dns entry for an raspberry pi)
+
+#define ILI9341_DRIVER //Large Display (that i've had on hand)
+                       //#define ST7735_DRIVER //smaller display (that i've had on hand)
+                       //#define noDisplay
